1fb00d630f0bbb0181766b0afce5d5e109700d79
Greg Vazquez <gvazquez@icstars.org>
2019-03-04 10:21:23 -0600

    Added heroku PORT

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.use('/api', event);
 app.use('/api', lesson);
 app.use('/api', file);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
